0c2e054b90abe919f207affb7e5d5cb30bfb103a
Kim Joseph <kimkimani016@gmail.com>
2022-12-23 15:11:57 +0300

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -30,7 +30,7 @@ export default function Post(props) {
 
 
 export async function getStaticPaths(){
-    const files = fs.readdirSync(path.join("posts"));
+    const files = fs.readdirSync("posts");
     const paths = files.map((file) => {
         return {
             params:{
